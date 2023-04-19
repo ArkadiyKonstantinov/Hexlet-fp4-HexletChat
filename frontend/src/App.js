7b2a781d7253c3f712c8e3ed4f05d83de442062e
Arkadiy Konstantinov <arkadiykonst@outlook.com>
2023-04-19 21:06:07 +0300

    Add auth context

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,51 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route, useLocation, Navigate } from 'react-router-dom';
 import Page404 from './components/Page404.jsx';
 import MainPage from './components/MainPage.jsx'
 import LoginPage from './components/LoginPage.jsx';
+import useAuth from './hooks/index.jsx';
+import AuthContext from './contexts/index.jsx';
+
+const AuthProvider = ({ children }) => {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const logIn = () => setLoggedIn(true);
+  const logOut = () => {
+    localStorage.removeItem('userId');
+    setLoggedIn(false);
+  };
+
+  return (
+    <AuthContext.Provider value={{ loggedIn, logIn, logOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+const PrivateRoute = ({ children }) => {
+  const auth = useAuth();
+  const location = useLocation();
+
+  return (
+    auth.loggedIn ? children : <Navigate to="/login" state={{ from: location }} />
+  );
+};
 
 const App = () => {
   return (
-  <BrowserRouter>
+  <AuthProvider>
+    <BrowserRouter>
       <Routes>
         <Route path='*' element={<Page404 />} />
-        <Route path='/' element={<MainPage />} />
+        <Route path='/' element={(
+          <PrivateRoute>
+            <MainPage />
+          </PrivateRoute>
+        )} />
         <Route path='/login' element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
+  </AuthProvider>
   );
 }
 
