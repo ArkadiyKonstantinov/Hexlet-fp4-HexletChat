c760150f5632104b61a7119fefbc53eb2c14e189
Arkadiy Konstantinov <arkadiykonst@outlook.com>
2023-04-22 15:06:52 +0300

    Refactoring LoginPage

--- frontend/src/components/LoginPage.jsx
+++ frontend/src/components/LoginPage.jsx
@@ -1,17 +1,11 @@
-import React, { useState } from "react";
-import Image from "react-bootstrap/Image";
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
-import Card from "react-bootstrap/Card";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import login from "../login.jpg";
+import React, { useState, useRef } from "react";
+import { Image, Button, Form, Card, Container, Row, Col } from 'react-bootstrap'
+import loginImg from "../assets/login.jpg";
 import * as Yup from "yup";
 import { useFormik } from "formik";
 import axios from "axios";
 import useAuth from "../hooks";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import { toast } from "react-toastify";
 import { routes } from "../routes.js";
 
@@ -28,7 +22,9 @@ const LoginPage = () => {
   const [authFailed, setAuthFailed] = useState(false);
 
   const auth = useAuth();
+  const location = useLocation();
   const navigate = useNavigate();
+  const usernameRef = useRef();
   const formik = useFormik({
     initialValues: {
       username: "admin",
@@ -43,21 +39,18 @@ const LoginPage = () => {
         .required("Required"),
     }),
     onSubmit: async (values) => {
-      const { username, password } = values;
+      setAuthFailed(false);
       try {
-        const res = await axios.post(routes.loginPath(), {
-          username,
-          password,
-        });
+        const res = await axios.post(routes.loginPath(), values);
         localStorage.setItem("userId", JSON.stringify(res.data));
-        setAuthFailed(false);
         auth.logIn();
-        navigate("/");
+        const { from } = location.state || { from: { pathname: '/' } };
+        navigate(from);
       } catch (err) {
         formik.setSubmitting(false);
         if (err.isAxiosError && err.code === "ERR_BAD_REQUEST") {
-          // console.dir(err)
           setAuthFailed(true);
+          usernameRef.current.select();
           return;
         }
         if (err.isAxiosError && err.code === "ERR_NETWORK") {
@@ -74,10 +67,10 @@ const LoginPage = () => {
       <Row className="justify-content-center align-content-center h-100">
         <Col className="col-12 col-md-8 col-xxl-6">
           <Card className="shadow-sm">
-            <Card.Body>
+            <Card.Body className="row p-5">
               <Row className="p-5">
                 <Col className="col-12 col-md-6 d-flex align-items-center justify-content-center">
-                  <Image src={login} roundedCircle="true" alt="Войти" />
+                  <Image src={loginImg} roundedCircle="true" alt="Войти" />
                 </Col>
                 <Col className="col-12 col-md-6 mt-3 mt-mb-0">
                   <Form onSubmit={formik.handleSubmit}>
@@ -87,11 +80,14 @@ const LoginPage = () => {
                         isInvalid={authFailed}
                         id="username"
                         name="username"
+                        autoComplete="username"
                         type="text"
                         onChange={formik.handleChange}
                         onBlur={formik.handleBlur}
                         value={formik.values.username}
+                        disabled={formik.isSubmitting}
                         placeholder="Ваш ник"
+                        ref={usernameRef}
                       />
                       <Form.Label htmlFor="username">Ваш ник</Form.Label>
                     </Form.Floating>
@@ -100,10 +96,12 @@ const LoginPage = () => {
                         isInvalid={authFailed}
                         id="password"
                         name="password"
+                        autoComplete="password"
                         type="password"
                         onChange={formik.handleChange}
                         onBlur={formik.handleBlur}
                         value={formik.values.password}
+                        disabled={formik.isSubmitting}
                         placeholder="Ваш пароль"
                       />
                       <Form.Label htmlFor="password">Пароль</Form.Label>
@@ -112,8 +110,10 @@ const LoginPage = () => {
                       </Form.Text>
                     </Form.Floating>
                     <Button
-                      className="w-100 mb-3 btn btn-outliner-primary"
+                      className="w-100 mb-3 btn"
+                      variant="outline-primary"
                       type="submit"
+                      disabled={formik.isSubmitting}
                     >
                       Войти
                     </Button>
