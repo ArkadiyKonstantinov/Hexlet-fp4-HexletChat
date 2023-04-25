7f69854f645a6918909c0387fbda4b47e71502bc
Arkadiy Konstantinov <arkadiykonst@outlook.com>
2023-04-25 15:39:44 +0300

    Fix SocketProvider

--- frontend/src/contexts/SocketContext.jsx
+++ frontend/src/contexts/SocketContext.jsx
@@ -5,27 +5,27 @@ import store from "../slices/index.js";
 import { messagesActions } from "../slices/messagesSlice.js";
 import { channelsActions } from "../slices/channelsSlice.js";
 
-const socket = io();
-const dispatch = store.dispatch;
-
-socket.on("newMessage", (messageWithId) => {
-  dispatch(messagesActions.addMessage(messageWithId));
-});
-
-socket.on("newChannel", (channelWithId) => {
-  dispatch(channelsActions.addChannel(channelWithId));
-  dispatch(channelsActions.setCurrentChannel(channelWithId.id));
-});
-
-socket.on("removeChannel", (data) => {
-  dispatch(channelsActions.removeChannel(data));
-});
-
-socket.on("renameChannel", (channel) => {
-  dispatch(channelsActions.renameChannel(channel));
-});
-
 const SocketProvider = ({ children }) => {
+  const socket = io();
+  const dispatch = store.dispatch;
+
+  socket.on("newMessage", (messageWithId) => {
+    dispatch(messagesActions.addMessage(messageWithId));
+  });
+
+  socket.on("newChannel", (channelWithId) => {
+    dispatch(channelsActions.addChannel(channelWithId));
+    dispatch(channelsActions.setCurrentChannel(channelWithId.id));
+  });
+
+  socket.on("removeChannel", (data) => {
+    dispatch(channelsActions.removeChannel(data));
+  });
+
+  socket.on("renameChannel", (channel) => {
+    dispatch(channelsActions.renameChannel(channel));
+  });
+
   return (
     <SocketContext.Provider value={{ socket }}>
       {children}
